43cfa8ea7e2ccc3e57d1f77377db003b2ad1dd11
sofiene-chouchen <chouchene.sofiene@outlook.com>
2023-12-18 19:29:24 +0100

    first commit

---
+++ src/main/java/com/polytechnique/JEE/project/JeeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.polytechnique.JEE.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polytechnique/JEE/project/JeeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polytechnique.JEE.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
